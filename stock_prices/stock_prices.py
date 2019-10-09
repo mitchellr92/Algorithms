6c730a0035187b7628a3e433a85d8987b2dd8c04
Mitchell Robles <mitchellr92@live.com>
2019-10-09 13:01:17 -0700

    find max profit passing test

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,27 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  
+  max_profit = 0
+
+  for i in range(len(prices)):
+    for j in range(i + 1, len(prices)):
+      profit = prices[j] - prices[i]
+      if profit > max_profit:
+        max_profit = profit
+
+
+  if max_profit == 0 and profit < 0:
+    return profit
+
+  return max_profit
+
+
+
+stocks = [100, 90, 80, 50, 20, 10]
+stocks2 = [1050, 270, 1540, 3800, 2]
+
+find_max_profit(stocks2)
 
 
 if __name__ == '__main__':
